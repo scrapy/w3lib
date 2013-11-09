751095ef9a528098c0a6078c12d5e7359d189315
Daniel Graña <dangra@gmail.com>
2013-11-09 02:57:01 -0200

    Bump version to 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='w3lib',
-    version='1.3',
+    version='1.4',
     license='BSD',
     description='Library of web-related functions',
     author='Scrapy project',
