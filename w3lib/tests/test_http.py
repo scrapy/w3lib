11779e416116625d3ccaeb04ff6367749b44db52
Mikhail Korobov <kmike84@gmail.com>
2014-07-21 23:58:02 +0600

    PY3 (maybe backwards incompatible) port headers_raw_to_dict and headers_dict_to_raw to Python 3.
    These functions now require bytestrings.

--- w3lib/tests/test_http.py
+++ w3lib/tests/test_http.py
@@ -1,5 +1,7 @@
 import unittest
-from w3lib.http import basic_auth_header
+from collections import OrderedDict
+from w3lib.http import (basic_auth_header,
+                        headers_dict_to_raw, headers_raw_to_dict)
 
 __doctests__ = ['w3lib.http'] # for trial support
 
@@ -11,3 +13,19 @@ class HttpTests(unittest.TestCase):
         # Check url unsafe encoded header
         self.assertEqual('Basic c29tZXVzZXI6QDx5dTk-Jm8_UQ==',
             basic_auth_header('someuser', '@<yu9>&o?Q'))
+
+    def test_headers_raw_to_dict(self):
+        raw = b"Content-type: text/html\n\rAccept: gzip\n\n"
+        dct = {b'Content-type': [b'text/html'], b'Accept': [b'gzip']}
+        self.assertEqual(headers_raw_to_dict(raw), dct)
+
+    def test_headers_dict_to_raw(self):
+        dct = OrderedDict([
+            (b'Content-type', b'text/html'),
+            (b'Accept', b'gzip')
+        ])
+        self.assertEqual(
+            headers_dict_to_raw(dct),
+            b'Content-type: text/html\r\nAccept: gzip'
+        )
+
