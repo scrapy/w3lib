ee44afc5da048f5da6a413fa78b4ba48832498a4
Mikhail Korobov <kmike84@gmail.com>
2014-08-14 03:56:58 +0600

    FIX w3lib.http.basic_auth_header should return bytes

--- w3lib/tests/test_http.py
+++ w3lib/tests/test_http.py
@@ -8,10 +8,10 @@ __doctests__ = ['w3lib.http'] # for trial support
 class HttpTests(unittest.TestCase):
 
     def test_basic_auth_header(self):
-        self.assertEqual('Basic c29tZXVzZXI6c29tZXBhc3M=',
+        self.assertEqual(b'Basic c29tZXVzZXI6c29tZXBhc3M=',
                 basic_auth_header('someuser', 'somepass'))
         # Check url unsafe encoded header
-        self.assertEqual('Basic c29tZXVzZXI6QDx5dTk-Jm8_UQ==',
+        self.assertEqual(b'Basic c29tZXVzZXI6QDx5dTk-Jm8_UQ==',
             basic_auth_header('someuser', '@<yu9>&o?Q'))
 
     def test_headers_raw_to_dict(self):
