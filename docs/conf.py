4ca13749e00cd5d6efd59a6f05d1ec094abacb3a
Paul Tremberth <paul.tremberth@gmail.com>
2016-04-11 17:26:35 +0200

    Bump version: 1.14.1 → 1.14.2

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ copyright = u'2014, w3lib developers'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.14.1'
+release = '1.14.2'
 # The short X.Y version.
 version = '.'.join(release.split('.')[:2])
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='w3lib',
-    version='1.14.1',
+    version='1.14.2',
     license='BSD',
     description='Library of web-related functions',
     author='Scrapy project',
