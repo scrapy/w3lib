a9a80b3c40677d11b9e2ddc5e59f5ee26d20c959
Paul Tremberth <paul.tremberth@gmail.com>
2017-02-08 18:49:19 +0100

    Bump version: 1.16.0 → 1.17.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='w3lib',
-    version='1.16.0',
+    version='1.17.0',
     license='BSD',
     description='Library of web-related functions',
     author='Scrapy project',
