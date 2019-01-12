3b7a3ff20ff5ea83aca30f568997f591b9674c03
Maram Sumanth <32808381+maramsumanth@users.noreply.github.com>
2019-01-12 18:15:55 +0530

    Update url.py

--- w3lib/url.py
+++ w3lib/url.py
@@ -35,7 +35,7 @@ EXTRA_SAFE_CHARS = b'|'  # see https://github.com/scrapy/w3lib/pull/25
 _safe_chars = RFC3986_RESERVED + RFC3986_UNRESERVED + EXTRA_SAFE_CHARS + b'%'
 
 def safe_url_string(url, encoding='utf8', path_encoding='utf8', quote_path=True):
-    """Convert the given URL into a legal URL by escaping unsafe characters 
+    """Convert the given URL into a legal URL by escaping unsafe characters
     according to RFC-3986.
 
     If a bytes URL is given, it is first converted to `str` using the given
