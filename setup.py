0c94d0cdc574c47138e8704eafdf382059b5d6d6
Adrián Chaves <adrian@chaves.io>
2020-05-13 20:53:36 +0200

    Bump version: 1.21.0 → 1.22.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='w3lib',
-    version='1.21.0',
+    version='1.22.0',
     license='BSD',
     description='Library of web-related functions',
     author='Scrapy project',
