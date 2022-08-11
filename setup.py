81b54a5ea894227a057297b8271e6577803295f7
Mikhail Korobov <kmike84@gmail.com>
2022-08-11 18:50:39 +0500

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="w3lib",
-    version="2.0.0",
+    version="2.0.1",
     license="BSD",
     description="Library of web-related functions",
     author="Scrapy project",
--- w3lib/__init__.py
+++ w3lib/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split("."))
