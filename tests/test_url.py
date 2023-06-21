6c6c29a9c79b6109d49d8957237ab1121ab1466f
Alexander Shadchin <shadchin@yandex-team.com>
2023-06-21 14:46:45 +0300

    Fix tests with Python 3.11.4+

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import unittest
 from inspect import isclass
@@ -386,7 +387,6 @@ KNOWN_SAFE_URL_STRING_URL_ISSUES = {
     "http://192.168.0.256",  # Invalid IP address
     "http://192.168.0.0.0",  # Invalid IP address / domain name
     "http://[2a01:5cc0:1:2::4]",  # https://github.com/scrapy/w3lib/issues/193
-    "http://[2a01:5cc0:1:2:3:4]",  # Invalid IPv6
     "https://example.com:",  # Removes the :
     # Does not convert \ to /
     "https://example.com\\a",
@@ -418,6 +418,10 @@ KNOWN_SAFE_URL_STRING_URL_ISSUES = {
     # (%) are not escaped.
     f"a://example.com#{FRAGMENT_TO_ENCODE}",
 }
+if sys.version_info < (3, 11, 4):
+    KNOWN_SAFE_URL_STRING_URL_ISSUES.add("http://[2a01:5cc0:1:2:3:4]")  # Invalid IPv6
+else:
+    KNOWN_SAFE_URL_STRING_URL_ISSUES.add(f"https://{USERNAME_TO_ENCODE}:{PASSWORD_TO_ENCODE}@example.com")
 
 
 @pytest.mark.parametrize(
