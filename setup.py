af8ed20f437e85bc6ec80b0c1fac08d420d38314
Andrey Rakhmatullin <wrar@wrar.name>
2023-08-03 12:47:07 +0400

    Bump version: 2.1.1 → 2.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="w3lib",
-    version="2.1.1",
+    version="2.1.2",
     license="BSD",
     description="Library of web-related functions",
     author="Scrapy project",
