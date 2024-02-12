63a2f2f8ef343f55f0372786af8449c2d88767b8
Adrián Chaves <adrian@chaves.io>
2024-02-12 16:04:33 +0100

    Initial use of Cython (x7 speedup)

--- setup.py
+++ setup.py
@@ -1,5 +1,39 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup, find_packages, Extension
 
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+extensions = [
+    Extension("w3lib._url", ["w3lib/_url.pyx"]),
+]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
 
 setup(
     name="w3lib",
@@ -20,6 +54,7 @@ setup(
     install_requires=[
         "idna",
     ],
+    ext_modules=extensions,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: BSD License",
