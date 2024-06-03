f41b2162520d8a8e4f7c3c99bdd5b601def425ca
Andrey Rakhmatullin <wrar@wrar.name>
2024-06-03 16:11:07 +0400

    Update tool versions, add description (#227)

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.rst", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="w3lib",
     version="2.1.2",
     license="BSD",
     description="Library of web-related functions",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Scrapy project",
     author_email="info@scrapy.org",
     url="https://github.com/scrapy/w3lib",
