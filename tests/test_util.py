1e7dd0c8e6bcd7c08ea228081146dce1cccd04f2
Andrey Rakhmatullin <wrar@wrar.name>
2025-01-25 22:43:10 +0500

    Remove deprecated functions.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,20 +1,8 @@
 from unittest import TestCase
 
-from pytest import deprecated_call, raises
+from pytest import raises
 
-from w3lib.util import (
-    str_to_unicode,
-    to_bytes,
-    to_native_str,
-    to_unicode,
-    unicode_to_str,
-)
-
-
-class StrToUnicodeTestCase(TestCase):
-    def test_deprecation(self):
-        with deprecated_call():
-            str_to_unicode("")
+from w3lib.util import to_bytes, to_unicode
 
 
 class ToBytesTestCase(TestCase):
@@ -23,19 +11,7 @@ class ToBytesTestCase(TestCase):
             to_bytes(True)  # type: ignore
 
 
-class ToNativeStrTestCase(TestCase):
-    def test_deprecation(self):
-        with deprecated_call():
-            to_native_str("")
-
-
 class ToUnicodeTestCase(TestCase):
     def test_type_error(self):
         with raises(TypeError):
             to_unicode(True)  # type: ignore
-
-
-class UnicodeToStrTestCase(TestCase):
-    def test_deprecation(self):
-        with deprecated_call():
-            unicode_to_str("")
