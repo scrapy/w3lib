d3c5ea8ebf8c8bca9d7be5352d560ca0b9addd72
Andrey Rakhmatullin <wrar@wrar.name>
2025-01-27 18:52:10 +0500

    Bump version: 2.2.1 → 2.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ copyright = "2014, w3lib developers"
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "2.2.1"
+release = "2.3.0"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
--- w3lib/__init__.py
+++ w3lib/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "2.2.1"
+__version__ = "2.3.0"
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split("."))
